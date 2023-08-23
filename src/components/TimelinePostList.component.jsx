c40ae9831df936aa59c56e0d19931e3f4f588906
MariaCLara <mariaclara130196@gmail.com>
2023-08-23 17:46:00 -0300

    feat/services-repost

--- src/components/TimelinePostList.component.jsx
+++ src/components/TimelinePostList.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import TimelinePostItem from "./TimelinePostItem.component";
 import useAuth from "../hooks/useAuth";
 import axios from "axios";
@@ -6,8 +6,8 @@ import loadingImage from "../assets/images/icons/loadingImage.gif";
 import { styled } from "styled-components";
 
 export default function TimelinePosts() {
-  const API_URL = process.env.REACT_APP_API_URL;
 
+  const API_URL = process.env.REACT_APP_API_URL;
   const { token } = useAuth();
   const config = { headers: { Authorization: `Bearer ${token}` } };
 
@@ -15,7 +15,13 @@ export default function TimelinePosts() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [emptyPage, setEmptyPage] = useState(false);
+  const [showRepost, setShowRepost] = useState(false);
+  const [sharing, setSharing] = useState(false);
+  const [repostCount, setRepostCount] = useState({});
+  const [selectedPostId, setSelectedPostId] = useState(null);
 
+
+  //CARREGAR POSTS
   useEffect(() => {
     console.log(token);
     axios
@@ -43,6 +49,55 @@ export default function TimelinePosts() {
         );
       });
   }, []);
+
+
+  // BUSCAR REPOSTS
+  const getRepost = useCallback(async () => {
+    try {
+      const result = await axios.get(`${API_URL}/posts/repost`, config);
+      const reposts = result.data;
+
+      const countRepost = {};
+      reposts.forEach((repost) => {
+        const { postId, reposts } = repost;
+        countRepost[postId] = reposts;
+      });
+
+      setRepostCount(countRepost);
+    } catch (error) {
+      console.error(error);
+      alert("An error occurred while fetching repost counts");
+    }
+  }, [config]);
+
+
+  //REPOSTAR
+    const postRepost = useCallback(async () => {
+      setSharing(true);
+  
+      try {
+        await axios.post(
+          `${API_URL}/posts/${selectedPostId}/repost`,
+          {},
+          config
+        );
+  
+        setRepostCount((prevRepostCount) => ({
+          ...prevRepostCount,
+          [selectedPostId]: (prevRepostCount[selectedPostId] || 0) + 1,
+        }));
+  
+        setShowRepost(false);
+      } 
+      catch (error) {
+        console.error(error);
+        alert("An error occurred while reposting the post");
+      } 
+      finally {
+        setSharing(false);
+      }
+    }, [config, selectedPostId]);
+
 
   return (
     <Container>
